7497ed52a46d5b3cab82457732e1cfe6351552cc
gustn <gustn5406@gmail.com>
2026-04-08 20:14:22 +0900

    [FEAT]두번째 수업 인스타 클론

---
+++ lib/insta_clone.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_study/main.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class InstaClone extends StatelessWidget {
+  const InstaClone({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -11,76 +11,155 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-          colorScheme: .fromSeed(seedColor: Colors.lightBlueAccent)),
-      home: const MyHomePage(title: '안녕하세요'),
+      home: const InstaClone(),
+      theme: ThemeData(scaffoldBackgroundColor: Colors.blue),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class InstaClone extends StatelessWidget {
+  const InstaClone({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      backgroundColor: Colors.white,
+      body: const SafeArea(
+        child: SingleChildScrollView(
+          child: Column(children: [HesderSection(), StorySection(),PostSection(),SizedBox(height: 20,),PostSection()]),
+        ),
+      ),
+      
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.black,
+        type: BottomNavigationBarType.fixed,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: const [
+          BottomNavigationBarItem(icon:Icon(Icons.home), label: ' '),
+          BottomNavigationBarItem(icon:Icon(Icons.home), label: ' '),
+          BottomNavigationBarItem(icon:Icon(Icons.home), label: ' '),
+          BottomNavigationBarItem(icon:Icon(Icons.home), label: ' '),
+          BottomNavigationBarItem(icon:Icon(Icons.home), label: ' '),
+        ],
+      ),
+    );
+  }
+}
 
-          children: [
+class HesderSection extends StatelessWidget {
+  const HesderSection({super.key});
 
-          Text('김현수',
-          style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),),
-        SizedBox(height: 10,),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(40), // 둥글기 정도
-              child: Image.asset('KakaoTalk_20260113_113643883.jpg', scale: 1.5, ),
-            )
-        ,
-
-        SizedBox(height: 10,),
-        Text('자기소개', style: TextStyle(fontWeight: FontWeight.bold),),
-        SizedBox(height: 10,),
-        Container(
-          padding: EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15), color: Color(0xAB98C7FF),),
-          child: Text(
-              '안녕하세요 저는 국민대학교 다니는 김현수 입니다 \n 현재 동아리 KOSS를 다니고 있으며, 플러터를 공부중입니다. \n 하고있는 스터디로는 플러터, 자료구조 파기초 입니다'
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: const Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Icon(Icons.add, color: Colors.black, size: 30),
+          Text(
+            'Instagram',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+            ),
           ),
+          Row(
+            children: [
+              Icon(Icons.bookmark_border, color: Colors.black, size: 28),
+              SizedBox(width: 16),
+              Icon(Icons.favorite_border, color: Colors.black, size: 28),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
 
+class StorySection extends StatelessWidget {
+  const StorySection({super.key});
+
+  Widget StoryItem(String name) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 14),
+      child: Column(
+        children: [
+          const CircleAvatar(radius: 32, backgroundColor: Colors.grey),
+          const SizedBox(height: 12),
+          Text(name, style: const TextStyle(color: Colors.black, fontSize: 13)),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 118,
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          children: [
+            StoryItem('내 스토리'),
+            StoryItem('hyeonsu2373'),
+            StoryItem('half_1313'),
+            StoryItem('jeryo_0'),
+            StoryItem('jimin._.0425'),
+            StoryItem('내 스토리'),
+            StoryItem('hyeonsu2373'),
+            StoryItem('half_1313'),
+            StoryItem('jeryo_0'),
+            StoryItem('jimin._.0425'),
+            StoryItem('내 스토리'),
+            StoryItem('hyeonsu2373'),
+            StoryItem('half_1313'),
+            StoryItem('jeryo_0'),
+            StoryItem('jimin._.0425'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PostSection extends StatelessWidget {
+  const PostSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(12, 10, 12, 10),
+          child: Row(children: [CircleAvatar(radius: 18, backgroundColor: Colors.grey),SizedBox(width: 10,),
+          Expanded(child: Text('hyeonsu', style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 16),)),
+          Icon(Icons.more_vert, color: Colors.black,)]),
         ),
 
+        Image.network( 'https://picsum.photos/500/400',
+          width: double.infinity, height: 420,fit: BoxFit.cover,),
+        Padding(
+          padding: const EdgeInsets.all(10),
+          child: Row(children: [Icon(Icons.favorite_border, color: Colors.black),
+          SizedBox(width: 12,),
+          Icon(Icons.mode_comment_outlined, color: Colors.black),
+          SizedBox(width: 12,),
+          Icon(Icons.send_outlined, color: Colors.black,)],),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Text('좋아요 : 200개',style: TextStyle(color: Colors.black,)),
+        )
 
-        Divider(indent: 150,
-          endIndent: 150,),
-        Center(
-          child: Row(mainAxisAlignment: MainAxisAlignment.center
-          ,children: [Icon(Icons.flag, size: 50,),
-          Icon(Icons.h_mobiledata_outlined, size: 50),
-          Icon(Icons.insert_chart, size: 50)],),
-      )
-
+        
       ],
-    ),)
-    ,
+
     );
   }
 }
